44904f75bff850019816bae256d7657f5f6f55dc
Pawan <pandeypawan2061@gmail.com>
2024-10-17 23:59:10 +0530

    fixing errors

--- ws/src/index.ts
+++ ws/src/index.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import { WebSocketServer } from "ws";
+import { ORDERBOOK } from "../../backend/src/data/dummy";
 const app = express();
 
 const server = app.listen(8080);
+console.log(JSON.stringify(ORDERBOOK), "here");
+const data = JSON.stringify(ORDERBOOK);
 
 const wss = new WebSocketServer({
   server: server,
 });
-
 wss.on("connection", (ws: WebSocket) => {
-  ws.send("wokringnvrn");
+  ws.send(data);
+
   console.log("wokring here");
 });
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(JSON.stringify(ORDERBOOK));
+    }
+  });
+}, 10000);
